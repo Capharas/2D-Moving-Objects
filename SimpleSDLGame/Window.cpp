c576a6eb541d99536dc0b75dcee92a589d8672ff
Phil <pgoeritz@live.de>
2018-03-21 19:35:39 +0100

    basic window impleneted

--- SimpleSDLGame/Window.cpp
+++ SimpleSDLGame/Window.cpp
@@ -2,12 +2,29 @@
 
 
 
-Window::Window(const string&, const int&, const int&)
+Window::Window(const string& TITLE, const int& WIN_WIDTH, const int& WIN_HEIGHT)
 {
+	m_window = SDL_CreateWindow(
+		TITLE.c_str(),
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		WIN_WIDTH,
+		WIN_HEIGHT,
+		SDL_WINDOW_SHOWN);
 
+	if (m_window == NULL) 
+	{
+		string* errorMessage = new string("Window could not be created! SDL_Error: %s\n");
+		errorMessage->append(SDL_GetError());
+
+		throw errorMessage;
+	}
+
+	m_screenSurface = SDL_GetWindowSurface(m_window);
 }
 
 
 Window::~Window()
 {
+	SDL_DestroyWindow(m_window);
 }
--- SimpleSDLGame/main.cpp
+++ SimpleSDLGame/main.cpp
@@ -3,10 +3,61 @@ Add Project description later
 */
 
 // includes
+#include <SDL.h>
+#include <string>
+#include <stdio.h>
+
+#include "Window.h"
 
 
+// Namespaces
+using std::string;
+
+
+// Global Vars
+const string TITLE = "Simple Game";
+const int SCREEN_HEIGHT = 400;
+const int SCREEN_WIDTH = 600;
+
+
+// Functions
+void sdlInit();
+
+
+// Main
 int main( int argc, char* args[])
 {
+	try 
+	{
+		// Init SDL Components
+		sdlInit();
+
+		// Create Window
+		Window win(TITLE, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+		// wait for 2 sec
+		SDL_Delay(2000);
+
+	}
+	catch (string* eMsg) 
+	{
+		printf(eMsg->c_str());
+	}
+
+	// Shut down components
+	SDL_Quit();
+
+	return 0;
+}
 
 
+void sdlInit() 
+{
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) 
+	{
+		string* errorMsg = new string("SDL could not initialize! SDL_Error: %s\n");
+		errorMsg->append(SDL_GetError());
+
+		throw errorMsg;
+	}
 }
